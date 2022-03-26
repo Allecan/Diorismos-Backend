a6d650324a67203f5a1a2968080ba12308656d7c
Allecan <allecan52@gmail.com>
2022-03-26 12:13:06 -0600

    Iniciando proyecto con Spring Boot
    Se creo la estructura principal del proyecto de Diorismos

---
+++ DiorismosAPI/src/main/java/com/api/diorismos/DiorismosApplication.java
@@ -0,0 +1,12 @@
+package com.api.diorismos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiorismosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiorismosApplication.class, args);
+    }
+}
